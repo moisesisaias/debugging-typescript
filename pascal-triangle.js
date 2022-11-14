14e60a6ee85465862f19200e5409bb8cbb885ed9
Moisés R. Ramirez <mramon@neural.com.do>
2022-11-14 11:58:55 -0400

    fixing two issues, missing 1

--- pascal-triangle.js
+++ pascal-triangle.js
@@ -8,6 +8,12 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
  1 3 3 1
 1 4 6 4 1
 
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+
 Examples:
 -
   Input: rowIndex = 3
@@ -31,9 +37,9 @@ var generatePascalTriangle = function(numRows) {
   // Create an array list to store the output result...
   let triangleAsArrayOfArrays = [];
   // For generating each row of the triangle...
-  for (currentRow = 0; currentRow < numRows; currentRow++) {
+  for (currentRow = 0; currentRow <= numRows; currentRow++) {
       triangleAsArrayOfArrays.push(Array(currentRow + 1));       // Initialize the first row of the pascal triangle as {1}...
-      for (column = 0; column < currentRow; column++) {
+      for (column = 0; column <= currentRow; column++) {
 
           if (column === 0 || column === currentRow) {
               triangleAsArrayOfArrays[currentRow][column] = 1;
@@ -47,8 +53,29 @@ var generatePascalTriangle = function(numRows) {
   return triangleAsArrayOfArrays;      // Return the output list of pascal values...
 };
 
-// console.log(generatePascalTriangle(5));
+console.log(generatePascalTriangle(5));
 generatePascalTriangle(4).forEach(element => {
   console.log(element);
 });
 // console.log(generatePascalTriangle(33)[3]);
+// 0 1 2 length -1 
+// [1, 2, 3]
+// ['st', 'dt']
+// let a = [{ name: "Brian", age: 26 }, { name: "Elian"}];
+// let user = a[1];
+// console.log(a[0]);
+// console.log(a[0].name + " Perez");
+// console.log(a[0].age);
+// console.log("-------");
+// console.log(user);
+// console.log(user.name + " Enmanuel");
+// console.log(user.age);
+
+// let aa = [[1, 2], [3, 4], [5, 6]];
+// console.log(aa);
+// console.log(aa[0]);
+// // let secondArray = aa[1];
+// // console.log(secondArray);
+// // console.log(secondArray[1]);
+// console.log(aa[1]);
+// console.log(aa[1][1])
